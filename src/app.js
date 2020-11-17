03a2efa47d56e2065f85aef9ea1d1e0676f9074b
w-e-d <1271505916@qq.com>
2020-11-17 18:39:21 +0800

    refactor: 调整目录结构

--- src/app.js
+++ src/app.js
@@ -20,17 +20,18 @@ app.use(json())
 app.use(logger())
 app.use(require('koa-static')(__dirname + '/public'))
 
+
 app.use(views(__dirname + '/views', {
   extension: 'ejs'
 }))
 
 // logger
-app.use(async (ctx, next) => {
-  const start = new Date()
-  await next()
-  const ms = new Date() - start
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
-})
+// app.use(async (ctx, next) => {
+//   const start = new Date()
+//   await next()
+//   const ms = new Date() - start
+//   console.log(`${ctx.method} ${ctx.url} - ${ms}ms`)
+// })
 
 // routes
 app.use(index.routes(), index.allowedMethods())
